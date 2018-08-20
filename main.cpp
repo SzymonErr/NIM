1d693a6eb0d5e7fbeb8f3ed07ed45187f1ade891
JuanBeedee <97szymek@gmail.com>
2018-08-20 21:59:15 +0200

    Fixes in CMake

--- main.cpp
+++ main.cpp
@@ -7,11 +7,15 @@
 
 #include <iostream>
 #include <cstdlib>
+#include "Player.h"
+#include "Board.h"
 
 using namespace std;
 
 int main() {
+	Player player1;
 	cout << "Hello World!" << endl;
+	player1.AskForMove();
 	return 0;
 }
 
