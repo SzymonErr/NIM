ba5813c935f715de0ee0742ea402d0eb36d66ebf
JuanBeedee <97szymek@gmail.com>
2018-08-20 18:00:35 +0200

    We start here!

---
+++ main.cpp
@@ -0,0 +1,18 @@
+/*
+ * main.cpp
+ *
+ *  Created on: Aug 20, 2018
+ *      Author: szymon
+ */
+
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+int main() {
+	cout << "Hello World!" << endl;
+	return 0;
+}
+
+
